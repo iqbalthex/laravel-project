6854af4e90ab51c398efd322da68776b419ff67c
iqbalthex <iqbalariem232@gmail.com>
2023-09-13 16:44:36 +0700

    Prepare controller & view

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>index desu</h1>
+
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,9 @@
+@include('layouts.header')
+
+<main>
+  <div class="container">
+    @yield('content')
+  </div>
+</main>
+
+@include('layouts.footer')
