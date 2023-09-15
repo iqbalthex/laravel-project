512d3e98ba237cafb74cd6609fdc6e0bb553792e
iqbalthex <iqbalariem232@gmail.com>
2023-09-15 07:29:38 +0700

    Add categories & posts resource route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ {
+  CategoryController,
+  PostController,
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::resource('categories', CategoryController::class);
+Route::resource('posts', PostController::class);
